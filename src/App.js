161e8c5f167cd2f239a3cdd3b59c68d14d6ecf5d
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2023-11-14 22:37:00 +0530

    weather App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import summer from "./images/summer.png"
+import winter from "./images/winter.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const[lat, setLat] = useState(0)
+  const[long, setLong] = useState(0)
+  const [hemisphere, setHemisphere] = useState("")
+  const [month, setMonth] = useState( new Date().getMonth()+1)
+
+  
+
+  function getLocation(){
+      if(navigator.geolocation){
+          navigator.geolocation.getCurrentPosition((positions)=>{
+              setLat(positions.coords.latitude)
+              setLong(positions.coords.longitude)
+
+
+              if(positions.coords.latitude>0){
+                setHemisphere("Northen Hemisphere")
+              }
+              else if(positions.coords.latitude<0){
+                setHemisphere("Southern Hemisphere")
+              }
+              else{
+                setHemisphere("Equator")
+              }
+          })
+      }
+  }
+
+
+
+  return(
+    <div>
+        {/* <h1>Latitude: {lat}</h1>
+        <h1>Longitude: {long}</h1>
+        <h1>Hemisphere: {hemisphere}</h1>
+        <h1>Month: {month}</h1> */}
+
+
+
+      <button onClick={getLocation}>Get Location</button>
+      {
+           hemisphere!="" && (
+            (hemisphere=="Northen Hemisphere" && (month>=4 && month<=10))|| 
+            (hemisphere=="Southern Hemisphere" && (month<4 || month>10)) )
+           && (
+            <div>
+                <h1>It's Summer</h1>
+                <img src={summer} alt="Summer Season" />        
+            </div>
+           )
+      }
+
+      {
+           hemisphere!="" && ((hemisphere=="Northen Hemisphere" && (month<4 || month>10))  ||(hemisphere=="Southern Hemisphere" && (month>=4 && month<=10)))
+
+           && (
+            <div>
+                <h1>It's Winter</h1>
+                <img style={{width:"500px"}} src={winter} alt="Winter Season" />        
+            </div>
+           )
+      }
+
+
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+
+
+
+// equiator, 
+// rainy season,
